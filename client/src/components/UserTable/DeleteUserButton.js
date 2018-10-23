3a4c632cc6667a9d9c368c529b9f90eee838a438
Matt DiDomenico <babsonmatt@gmail.com>
2018-10-23 17:47:29 -0400

    refactors some components out of Manage

---
+++ client/src/components/UserTable/DeleteUserButton.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Button, Icon, Modal } from 'semantic-ui-react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { userFragment } from '../../fragments/user';
+
+const DELETE_USER_MUTATION = gql`
+  mutation DeleteUser($id: ID!) {
+    deleteUser(id: $id) {
+      id
+    }
+  }
+`;
+
+const GET_USERS_QUERY = gql`
+  query($filter: String) {
+    users(filter: $filter) {
+      ...User
+    }
+  }
+  ${userFragment}
+`;
+
+class DeleteUserButton extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { user, filter } = this.props;
+    return (
+      <React.Fragment>
+        <Button size="mini" color="red" onClick={this.handleOpenModal} icon>
+          <Icon name="delete" />
+        </Button>
+        <Modal size="mini" open={this.state.open}>
+          <Modal.Header>
+            Delete {user.firstName} {user.lastName}?
+          </Modal.Header>
+          <Modal.Content>
+            <p>
+              Are you sure you want to delete {user.firstName} {user.lastName}?
+            </p>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button onClick={this.handleCloseModal} negative>
+              No
+            </Button>
+            <Mutation
+              key={user.id}
+              mutation={DELETE_USER_MUTATION}
+              update={(cache, { data: { deleteUser } }) => {
+                const { users } = cache.readQuery({
+                  query: GET_USERS_QUERY,
+                  variables: { filter },
+                });
+                cache.writeQuery({
+                  query: GET_USERS_QUERY,
+                  variables: { filter },
+                  data: {
+                    users: users.filter(user => user.id !== deleteUser.id),
+                  },
+                });
+              }}
+            >
+              {(deleteUser, { data }) => (
+                <Button
+                  positive
+                  icon="checkmark"
+                  labelPosition="right"
+                  content="Yes"
+                  onClick={() => {
+                    deleteUser({ variables: { id: user.id } });
+                    this.handleCloseModal();
+                  }}
+                />
+              )}
+            </Mutation>
+          </Modal.Actions>
+        </Modal>
+      </React.Fragment>
+    );
+  }
+}
+
+export default DeleteUserButton;
---
+++ client/src/components/UserTable/UserTable.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Button, Table, Icon, Modal } from 'semantic-ui-react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import UserChart from '../../components/UserChart';
+import DeleteUserButton from './DeleteUserButton';
+import { userFragment } from '../../fragments/user';
+
+console.log(UserChart);
+console.log(DeleteUserButton);
+
+const GET_USERS_QUERY = gql`
+  query($filter: String) {
+    users(filter: $filter) {
+      ...User
+    }
+  }
+  ${userFragment}
+`;
+
+const UserTable = ({ filter }) => (
+  <Query query={GET_USERS_QUERY} variables={{ filter }}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return (
+        <Table celled striped>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>First Name</Table.HeaderCell>
+              <Table.HeaderCell>Last Name </Table.HeaderCell>
+              <Table.HeaderCell>Email</Table.HeaderCell>
+              <Table.HeaderCell />
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {data.users.map(user => (
+              <Table.Row key={user.id}>
+                <Table.Cell>{user.firstName}</Table.Cell>
+                <Table.Cell>{user.lastName}</Table.Cell>
+                <Table.Cell>{user.email}</Table.Cell>
+                <Table.Cell collapsing textAlign="right">
+                  <Modal
+                    trigger={
+                      <Button size="mini" icon>
+                        <Icon name="chart line" />
+                      </Button>
+                    }
+                  >
+                    <Modal.Header>
+                      {user.firstName} {user.lastName}
+                      's Chart
+                    </Modal.Header>
+                    <Modal.Content>
+                      <Modal.Description>
+                        <UserChart
+                          data={user.results}
+                          name={`${user.firstName} ${user.lastName}`}
+                        />
+                      </Modal.Description>
+                    </Modal.Content>
+                  </Modal>
+                  <DeleteUserButton user={user} filter={filter} />
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      );
+    }}
+  </Query>
+);
+
+export default UserTable;
